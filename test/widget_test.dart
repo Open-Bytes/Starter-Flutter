c2b0610ce72aa14b7a1c4b0edc54f19f7f7fb8c7
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-01-29 17:25:20 +0200

    remove testWidgets from main tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:starterflutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainScreen());
+    await tester.pumpWidget(MainScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
